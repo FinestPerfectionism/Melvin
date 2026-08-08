9627161d26e05a2a880b07dba04ac00639a9664b
saltgranule <leerowland596@gmail.com>
2026-08-08 02:08:00 +0100

    fixed prv cog

--- cogs/private.py
+++ cogs/private.py
@@ -3,50 +3,48 @@ from ui import ResponseUI
 import discord
 from discord.ext import commands
 from discord import app_commands
+
 primary = f"#{PRIMARY}"
 
 
 class PrivateCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.log_channel = self.bot.get_channel(LOG_CHANNEL)
 
     # app commands will eventually go here
     private = app_commands.Group(name="private", description="some private stuff, gated")
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        if self.log_channel is None:
+        log_channel = self.bot.get_channel(LOG_CHANNEL)
+        if log_channel is None:
             return
 
         view = ResponseUI()
         view.text_display.content = f"**Melvin was just added to {guild.name}**"
-
-        if guild.icon:
-            view.container.add_item(
-                discord.ui.MediaGallery(discord.MediaGalleryItem(media=guild.icon.url))
-            )
+        view.container.add_item(
+            discord.ui.MediaGallery(discord.MediaGalleryItem(media='https://files.catbox.moe/0rgmtr.png'))
+        )
 
         try:
-            await self.log_channel.send(view=view, allowed_mentions=discord.AllowedMentions.none())
+            await log_channel.send(view=view, allowed_mentions=discord.AllowedMentions.none())
         except (discord.Forbidden, discord.HTTPException):
             pass
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        if self.log_channel is None:
+        log_channel = self.bot.get_channel(LOG_CHANNEL)
+        if log_channel is None:
             return
 
         view = ResponseUI()
         view.text_display.content = f"**Melvin was just removed from {guild.name}**"
-
-        if guild.icon:
-            view.container.add_item(
-                discord.ui.MediaGallery(discord.MediaGalleryItem(media='https://files.catbox.moe/0rgmtr.png'))
-            )
+        view.container.add_item(
+            discord.ui.MediaGallery(discord.MediaGalleryItem(media='https://files.catbox.moe/0rgmtr.png'))
+        )
 
         try:
-            await self.log_channel.send(view=view, allowed_mentions=discord.AllowedMentions.none())
+            await log_channel.send(view=view, allowed_mentions=discord.AllowedMentions.none())
         except (discord.Forbidden, discord.HTTPException):
             pass
 
